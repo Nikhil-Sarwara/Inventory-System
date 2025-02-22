cf2912817933f0b37fff80b69fa5da36fac410b2
Nikhil Sarwara <117506262+Nikhil-Sarwara@users.noreply.github.com>
2025-02-23 01:28:15 +1100

    ignore uploads folder in version control

--- handle_git.js
+++ handle_git.js
@@ -1,8 +1,10 @@
 const fs = require("fs");
 const path = require("path");
+
 /**
- * Creates a .gitignore file if it does not already exist in the current directory or parent directory.
- * The created .gitignore file will contain entries to ignore node_modules and .env files.
+ * Creates or updates a .gitignore file if it does not already exist in the current directory or parent directory,
+ * or if the existing .gitignore file does not contain the desired entries.
+ * The .gitignore file will contain entries to ignore node_modules and .env files.
  */
 function createGitignoreIfNotExists() {
   const currentDir = process.cwd();
@@ -10,17 +12,32 @@ function createGitignoreIfNotExists() {
   const currentGitignore = path.join(currentDir, ".gitignore");
   const parentGitignore = path.join(parentDir, ".gitignore");
 
-  if (!fs.existsSync(currentGitignore) && !fs.existsSync(parentGitignore)) {
-    fs.writeFileSync(
-      currentGitignore,
-      `
+  const gitignoreContent = `
 # ignore node_modules folder
 /node_modules
 
+/uploads
+
 # ignore .env file
 /.env
-`
-    );
+`.trim();
+
+  const doesGitignoreNeedUpdate = (filePath) => {
+    if (!fs.existsSync(filePath)) {
+      return true;
+    }
+    const existingContent = fs.readFileSync(filePath, "utf8");
+    return !existingContent.includes(gitignoreContent);
+  };
+
+  const updateGitignore = (filePath) => {
+    fs.writeFileSync(filePath, gitignoreContent);
+  };
+
+  if (doesGitignoreNeedUpdate(currentGitignore)) {
+    updateGitignore(currentGitignore);
+  } else if (doesGitignoreNeedUpdate(parentGitignore)) {
+    updateGitignore(parentGitignore);
   }
 }
 
